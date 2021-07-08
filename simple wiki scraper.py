e034ddb15a6866d15bcee572c9286f873d38e1d6
AlexeySadykhov <81092836+AlexeySadykhov@users.noreply.github.com>
2021-07-08 23:53:43 +0300

    Update simple wiki scraper.py

--- simple wiki scraper.py
+++ simple wiki scraper.py
@@ -1,16 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def get_names(URL):
     page_response = requests.get(URL, timeout=5)
     page_content = BeautifulSoup(page_response.content, "html.parser")
-    array = [ ]
+    array = []
     for i in page_content.find_all('div', {'class': 'div-col'}):
         links = i.find_all('a', href=True)
         for link in links:
             title = link.get('title')
             array.append(title)
     return array
+
 
 url = 'https://en.wikipedia.org/wiki/List_of_current_automobile_marques'
 print(get_names(url))
