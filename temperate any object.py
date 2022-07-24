ce2dc57a511f4b85049891ea48fce3dee24360a3
AlexeySadykhov <81092836+AlexeySadykhov@users.noreply.github.com>
2022-07-24 22:45:51 +0300

    Update temperate any object.py

--- temperate any object.py
+++ temperate any object.py
@@ -1,3 +1,12 @@
+#######################################################################################################################
+# This script has been created for music composers. Let us say, if we work with a tempered scale                      #
+# it means that our pitch organization obeys to the determined system with its own logic.                             #
+# Sometimes composers want to apply these rules to other parameters of the sound, not only to the pitch.              #
+# For example, in 12-tone music it was very popular method: some composers used to create a tempered scale of tempos. #
+# This utility allows you to use it with anything you want.                                                           #
+# You only need to specify a start point of your parameter and a number of iterations you want to get.                #
+#######################################################################################################################
+
 start_point = float(input('Enter start point:'))
 num_of_iter = int(input('Enter number of iterations:'))
 
