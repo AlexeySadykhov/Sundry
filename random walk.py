d96a6d4971b9da4e0562c8189f5d4826be640b5e
AlexeySadykhov <81092836+AlexeySadykhov@users.noreply.github.com>
2022-07-24 21:58:15 +0300

    Update and rename random wandering.py to random walk.py

---
+++ random walk.py
@@ -0,0 +1,28 @@
+################################################################
+# It is just a simple random walk generator.                   #
+# Use it if you want to see beautiful pictures of random walk, #
+# which sometimes looks like a cryptocurrency rate =).         #
+################################################################
+
+import random
+import matplotlib.pyplot as plt
+
+
+steps = [1, -1]
+walk = []
+i = 0
+while i != 100:
+    i += random.choice(steps)
+    walk.append(i)
+
+plt.plot([n for n in range(len(walk))],
+         walk)
+plt.show()
+
+ans = input('Would you like to save a text file? (y/n)')
+if ans == 'y':
+    file = open('random_walk.txt', 'w')
+    for x in walk:
+        file.write(str(x)+'\n')
+    file.close()
+print('Done')
